864d534735b7d3ff407a21ece2433b6ec0fde37d
Melissa Wahnish <melissawahnish@gmail.com>
2016-04-07 14:34:34 -0400

    wip

--- spec/features/create_posts_spec.rb
+++ spec/features/create_posts_spec.rb
@@ -8,17 +8,6 @@ feature "user creates blog post" do
 		fill_in "post[author]", with: Faker::Book.author
 		
 		find('button[data-cmd="underline"]').click
-		find('button[data-cmd="insertImage"]').click
-		# within(".fr-form") do
-		# 	expect(page).to have_selector "input"
-		# end	
-		# pic_path = 'spec/fixtures/unit2_lesson6.jpg'
-		
-		# within(".fr-popup") do 
-		# 	attach_file "input[type=file]", pic_path
-		# end	
-	
-		
 		
 	  find(:css, ".fr-element.fr-view").set("This is a great post.")
 		click_button "Create Post"
@@ -26,8 +15,8 @@ feature "user creates blog post" do
 		expect(Post.count).to eq 1
 		post = Post.last
 		expect(page).to have_text(post.title)
-		
 		expect(page).to have_selector :css, "div.fr-view"
+		expect(page).to have_css("p", value: "This is a great post.")
 
 	end	
 
